229a594ba3384c35b0007d9ff7dbbd974df5d7a2
Kevin P. Dyer <kpdyer@gmail.com>
2015-12-06 08:04:25 -0800

    make example actually work

--- example.py
+++ example.py
@@ -1,20 +1,20 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-import FFX
+import ffx
 
 
 radix = 2
 blocksize = 2 ** 10
 
-ffx = FFX.new(radix=radix)
+K = ffx.FFXInteger('0' * 128, radix=radix, blocksize=128)
+T = ffx.FFXInteger('0' * blocksize, radix=radix, blocksize=blocksize)
+X = ffx.FFXInteger('0' * blocksize, radix=radix, blocksize=blocksize)
 
-K = FFX.FFXInteger('0' * 128, radix=radix, blocksize=128)
-T = FFX.FFXInteger('0' * blocksize, radix=radix, blocksize=blocksize)
-X = FFX.FFXInteger('0' * blocksize, radix=radix, blocksize=blocksize)
+ffx_obj = ffx.new(K.to_bytes(16), radix=radix)
 
-C = ffx.encrypt(K, T, X)
-Y = ffx.decrypt(K, T, C)
+C = ffx_obj.encrypt(T, X)
+Y = ffx_obj.decrypt(T, C)
 
 print X
 print C
