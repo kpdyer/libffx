027b24657a3afbc7afba293619dcf3a602f12fa1
Sergey N. Belinsky <sergeybe@gmail.com>
2018-10-29 17:32:39 +0500

    Change requires from pycrypto to pycryptodome

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 
 setup(
     name='ffx',
-    install_requires=['gmpy', 'pycrypto', 'six'],
+    install_requires=['gmpy', 'pycryptodome', 'six'],
     test_suite='ffx.tests',
     version='0.0.2',
     description='FFX',
