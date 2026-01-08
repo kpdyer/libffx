72a5a268f82bed69848e109147dad379dda78778
Kevin P. Dyer <kpdyer@dyer.io>
2026-01-07 18:39:53 -0800

    Modernize core library for Python 3
    - Replace gmpy with gmpy2 (maintained, Python 3 compatible)
    - Replace pycrypto with pycryptodome (actively maintained)
    - Fix bytes/string handling for Python 3
    - Remove 'long' type (unified with int in Python 3)
    - Replace deprecated __getslice__ with __getitem__ slicing
    - Add type hints throughout
    - Add docstrings and __all__ exports
    - Improve code organization and naming

--- ffx/__init__.py
+++ ffx/__init__.py
@@ -1,75 +1,125 @@
-import string
-import math
-import binascii
+"""
+FFX - Format Preserving Encryption
 
-import gmpy as gmpy
+A Python implementation of The FFX Mode of Operation for Format-Preserving Encryption.
+Based on the NIST proposal: http://csrc.nist.gov/groups/ST/toolkit/BCM/documents/proposedmodes/ffx/ffx-spec2.pdf
+
+This implementation uses FFX-A2 with:
+- Maximally-balanced Feistel structure
+- 10 rounds (constant, independent of message size)
+- AES-128 as the underlying block cipher
+"""
+
+from __future__ import annotations
+
+import math
+import string
+from typing import Union
+
+import gmpy2
 
 from Crypto.Cipher import AES
 
-_gmpy_mpz_type = type(gmpy.mpz(0))
-_gmpy_mpf_type = type(gmpy.mpf(0))
+__all__ = ['new', 'FFXInteger', 'FFXEncrypter', 'InvalidRadixException', 'UnknownTypeException']
 
 
-def new(K, radix):
-    return FFXEncrypter(K, radix)
-
-
-class Bottom(Exception):
-    pass
+def new(key: bytes, radix: int) -> FFXEncrypter:
+    """Create a new FFX encrypter with the given key and radix.
+    
+    Args:
+        key: 16-byte AES key
+        radix: Base for the message alphabet (2-36)
+    
+    Returns:
+        FFXEncrypter instance
+    """
+    return FFXEncrypter(key, radix)
 
 
 class UnknownTypeException(Exception):
+    """Raised when an unsupported type is passed to FFXInteger."""
     pass
 
 
 class InvalidRadixException(Exception):
+    """Raised when an invalid radix is specified (must be 2-36)."""
     pass
 
 
-def long_to_bytes(N, blocksize=1):
-    """Given an input integer ``N``, ``long_to_bytes`` returns the representation of ``N`` in bytes.
-    If ``blocksize`` is greater than ``1`` then the output string will be right justified and then padded with zero-bytes,
-    such that the return values length is a multiple of ``blocksize``.
+def long_to_bytes(n: int, blocksize: int = 1) -> bytes:
+    """Convert an integer to bytes representation.
+    
+    Args:
+        n: Integer to convert
+        blocksize: Minimum output length (will be padded with zero bytes)
+    
+    Returns:
+        Bytes representation of n, padded to blocksize
     """
-
-    if type(N) == FFXInteger:
-        return N.to_bytes()
+    if isinstance(n, FFXInteger):
+        return n.to_bytes()
     
-    bytestring = gmpy.digits(N, 16)
-    bytestring = '0' + bytestring if (len(bytestring) % 2) != 0 else bytestring
-    bytestring = binascii.unhexlify(bytestring)
+    if n == 0:
+        byte_string = b'\x00'
+    else:
+        hex_string = gmpy2.digits(n, 16)
+        if len(hex_string) % 2:
+            hex_string = '0' + hex_string
+        byte_string = bytes.fromhex(hex_string)
+    
+    if blocksize > 0 and (len(byte_string) % blocksize) != 0:
+        byte_string = b'\x00' * (blocksize - (len(byte_string) % blocksize)) + byte_string
+    
+    return byte_string
 
-    if blocksize>0 and (len(bytestring) % blocksize) != 0:
-        bytestring = '\x00' * \
-            (blocksize - (len(bytestring) % blocksize)) + bytestring
 
-    return bytestring
+def bytes_to_long(byte_string: bytes) -> int:
+    """Convert bytes to integer representation.
+    
+    Args:
+        byte_string: Bytes to convert
+    
+    Returns:
+        Integer representation
+    """
+    return int.from_bytes(byte_string, byteorder='big')
 
-def bytes_to_long(bytestring):
-    """Given a ``bytestring`` returns its integer representation ``N``.
+
+class FFXInteger:
+    """Integer representation for FFX operations with a specific radix and blocksize.
+    
+    This class represents an integer in a given radix with optional zero-padding
+    to a specified blocksize.
     """
     
-    N = binascii.hexlify(bytestring)
-    N = gmpy.mpz(N, 16)
-    
-    return N
+    _gmpy_mpz_type = type(gmpy2.mpz(0))
+    _gmpy_mpfr_type = type(gmpy2.mpfr(0))
 
-
-class FFXInteger(object):
-
-    def __init__(self, x, radix=2, blocksize=None):
-        _x_type = type(x)
+    def __init__(
+        self, 
+        x: Union[int, str, 'FFXInteger', float], 
+        radix: int = 2, 
+        blocksize: int | None = None
+    ):
+        """Initialize an FFXInteger.
         
-        if _x_type in [_gmpy_mpz_type, int, long]:
-            self._x = gmpy.digits(x, radix)
-        elif _x_type in [FFXInteger]:
+        Args:
+            x: Value to convert (int, str representation in radix, or FFXInteger)
+            radix: Base for string representation (2-36)
+            blocksize: Minimum length of string representation (zero-padded)
+        """
+        x_type = type(x)
+        
+        if x_type in (self._gmpy_mpz_type, int):
+            self._x = gmpy2.digits(x, radix)
+        elif x_type is FFXInteger:
             self._x = x._x
-        elif _x_type in [str]:
+        elif x_type is str:
             self._x = x
-        elif _x_type in [float, _gmpy_mpf_type]:
-            self._x = gmpy.digits(gmpy.mpz(x), radix)
+        elif x_type in (float, self._gmpy_mpfr_type):
+            self._x = gmpy2.digits(gmpy2.mpz(x), radix)
         else:
-            raise UnknownTypeException(type(x))
+            raise UnknownTypeException(f"Unsupported type: {type(x)}")
 
         self._radix = radix
         if blocksize:
@@ -78,210 +128,272 @@ class FFXInteger(object):
         else:
             self._blocksize = None
 
-        self._as_bytes = None
-        self._as_int = None
-        self._len = None
+        self._as_bytes: bytes | None = None
+        self._as_int: int | None = None
+        self._len: int | None = None
 
-    def __add__(self, other):
-        retval = self.to_int()
-        if type(other) == FFXInteger:
-            retval += other.to_int()
+    def __add__(self, other: Union['FFXInteger', int]) -> int:
+        result = self.to_int()
+        if isinstance(other, FFXInteger):
+            result += other.to_int()
         else:
-            retval += other
-        return retval
+            result += other
+        return result
 
-    def __sub__(self, other):
-        retval = self.to_int()
-        if type(other) == FFXInteger:
-            retval -= other.to_int()
+    def __radd__(self, other: int) -> int:
+        return other + self.to_int()
+
+    def __sub__(self, other: Union['FFXInteger', int]) -> int:
+        result = self.to_int()
+        if isinstance(other, FFXInteger):
+            result -= other.to_int()
         else:
-            retval -= other
-        return retval
+            result -= other
+        return result
 
-    def __mod__(self, other):
-        retval = self.to_int()
-        if type(other) == FFXInteger:
-            retval %= other.to_int()
+    def __rsub__(self, other: int) -> int:
+        return other - self.to_int()
+
+    def __mod__(self, other: Union['FFXInteger', int]) -> int:
+        result = self.to_int()
+        if isinstance(other, FFXInteger):
+            result %= other.to_int()
         else:
-            retval %= other
-        return retval
+            result %= other
+        return result
 
-    def __eq__(self, other):
-        if type(other) == FFXInteger:
-            retval = self.to_int() == other.to_int()
-        elif type(other) in [str]:
-            retval = (self._x == other)
-        elif type(other) in [int]:
-            retval = (self.to_int() == other)
-        elif type(other) in [type(None)]:
-            retval = False
+    def __eq__(self, other: object) -> bool:
+        if isinstance(other, FFXInteger):
+            return self.to_int() == other.to_int()
+        elif isinstance(other, str):
+            return self._x == other
+        elif isinstance(other, int):
+            return self.to_int() == other
+        elif other is None:
+            return False
         else:
-            raise UnknownTypeException()
-        return retval
+            raise UnknownTypeException(f"Cannot compare FFXInteger with {type(other)}")
 
-    def __len__(self):
-        if self._len == None:
+    def __hash__(self) -> int:
+        return hash(self.to_int())
+
+    def __len__(self) -> int:
+        if self._len is None:
             self._len = len(self._x)
         return self._len
 
-    def __getitem__(self, i):
-        return FFXInteger(self._x[i], self._radix, 1)
+    def __getitem__(self, key: Union[int, slice]) -> 'FFXInteger':
+        if isinstance(key, slice):
+            sliced = self._x[key]
+            return FFXInteger(sliced, self._radix, len(sliced))
+        return FFXInteger(self._x[key], self._radix, 1)
 
-    def __getslice__(self, i, j):
-        return FFXInteger(self._x[i:j], self._radix, len(self._x[i:j]))
-
-    def __str__(self):
+    def __str__(self) -> str:
         return self._x
-    
-    def __repr__(self):
-        return self.to_str()
 
-    def to_int(self):
-        if not self._as_int:
+    def __repr__(self) -> str:
+        return f"FFXInteger('{self._x}', radix={self._radix}, blocksize={self._blocksize})"
+
+    def to_int(self) -> int:
+        """Convert to integer."""
+        if self._as_int is None:
             self._as_int = int(self._x, self._radix)
         return self._as_int
 
-    def to_bytes(self, blocksize=None):
+    def to_bytes(self, blocksize: int | None = None) -> bytes:
+        """Convert to bytes representation.
+        
+        Args:
+            blocksize: Override the output size in bytes
+        
+        Returns:
+            Bytes representation
+        """
         if blocksize is None and self._blocksize is not None:
-            blocksize = (self._radix ** self._blocksize - 1).bit_length() / 8
-        if not self._as_bytes:
+            blocksize = int(math.ceil((self._radix ** self._blocksize - 1).bit_length() / 8))
+        
+        if self._as_bytes is None or blocksize is not None:
             if blocksize is None:
                 blocksize = 1
-                if self.to_int()>0:
-                    blocksize = self.to_int().bit_length()
-                blocksize /= 8
-            self._as_bytes = long_to_bytes(self.to_int(), blocksize=blocksize)
+                if self.to_int() > 0:
+                    blocksize = (self.to_int().bit_length() + 7) // 8
+            result = long_to_bytes(self.to_int(), blocksize=blocksize)
+            if self._as_bytes is None:
+                self._as_bytes = result
+            return result
+        
         return self._as_bytes
 
-    def to_str(self):
+    def to_str(self) -> str:
+        """Return string representation in the current radix."""
         return self._x
 
 
-class FFXEncrypter(object):
+class FFXEncrypter:
+    """FFX Mode of Operation Encrypter.
+    
+    Implements the FFX-A2 algorithm for format-preserving encryption.
+    """
+    
+    # Number of Feistel rounds (constant per FFX-A2 spec)
+    NUM_ROUNDS = 10
 
-    def __init__(self, K, radix):
+    def __init__(self, key: bytes, radix: int):
+        """Initialize the FFX encrypter.
+        
+        Args:
+            key: 16-byte AES-128 key
+            radix: Base for the message alphabet (2-36)
+        
+        Raises:
+            InvalidRadixException: If radix is not in range 2-36
+        """
         if radix not in range(2, 37):
-            raise InvalidRadixException()
+            raise InvalidRadixException(f"Radix must be between 2 and 36, got {radix}")
 
-        self._radix = gmpy.mpz(radix)
-        self._chars = string.digits + string.ascii_lowercase
-        self._chars = self._chars[:radix]
-        _chars = []
-        for c in self._chars:
-            _chars.append(c)
-        self._chars = _chars
+        self._radix = gmpy2.mpz(radix)
+        self._chars = (string.digits + string.ascii_lowercase)[:radix]
+        
+        self._key = key
+        self._ecb = AES.new(key, AES.MODE_ECB)
+        self._P_cache: dict[int, bytes] = {}
 
-        self._K = K
-        self._ecb = AES.new(K, AES.MODE_ECB)
-        self._P = {}
+    @staticmethod
+    def _is_even(n: int) -> bool:
+        """Check if n is even."""
+        return (n & 1) == 0
 
-    def isEven(self, n):
-        return not ((n & 0x1) == 1)
+    def _add_mod(self, x: FFXInteger, y: int) -> FFXInteger:
+        """Add x + y modulo radix^blocksize."""
+        result = (x + y) % (x._radix ** x._blocksize)
+        return FFXInteger(result, radix=int(self._radix), blocksize=x._blocksize)
 
-    def add(self, X, Y):
-        retval = (X + Y) % (X._radix ** (X._blocksize))
-        return FFXInteger(retval, radix=self._radix, blocksize=X._blocksize)
+    def _sub_mod(self, x: FFXInteger, y: int) -> FFXInteger:
+        """Subtract x - y modulo radix^blocksize."""
+        result = (x - y) % (x._radix ** x._blocksize)
+        return FFXInteger(result, radix=int(self._radix), blocksize=x._blocksize)
 
-    def sub(self, X, Y):
-        retval = (X - Y) % (X._radix ** (X._blocksize))
-        return FFXInteger(retval, radix=self._radix, blocksize=X._blocksize)
+    @staticmethod
+    def _split(n: int) -> int:
+        """Calculate the split point for Feistel network (maximally-balanced)."""
+        return n // 2
 
-    #def rnds(self, n):
-    #    return 10
-
-    def split(self, n):
-        """TODO"""
-        return int(math.floor((n * 1.0) / 2))
-
-    def F(self, n, T, i, B):
-        if T == 0:
+    def _F(self, n: int, tweak: Union[FFXInteger, int], i: int, b: FFXInteger) -> int:
+        """The round function F for the Feistel network.
+        
+        Implements the PRF per FFX-A2 specification.
+        
+        Args:
+            n: Total message length
+            tweak: The tweak value
+            i: Round number
+            b: Right half of the current state
+        
+        Returns:
+            Output of the round function
+        """
+        if tweak == 0:
             t = 0
         else:
-            t = len(T)
+            t = len(tweak)
 
         beta = math.ceil(n / 2.0)
-        b = int(math.ceil(math.ceil(beta * math.log(self._radix, 2)) / 8.0))
-        d = 4 * int(math.ceil(b / 4.0))
+        b_bytes = int(math.ceil(math.ceil(beta * math.log(int(self._radix), 2)) / 8.0))
+        d = 4 * int(math.ceil(b_bytes / 4.0))
 
-        if self.isEven(i):
-            m = int(math.floor(n / 2.0))
+        if self._is_even(i):
+            m = n // 2
         else:
             m = int(math.ceil(n / 2.0))
 
-        if not self._P.get(n):
-            P = '\x01'  # vers
-            P += '\x02'  # method
-            P += '\x01'  # addition
-            P += long_to_bytes(self._radix, 3)
-            P += '\x0a' # always ten
-            P += long_to_bytes(self.split(n)%256, 1)
+        # Build P (cached per message length)
+        if n not in self._P_cache:
+            P = b'\x01'  # vers
+            P += b'\x02'  # method
+            P += b'\x01'  # addition
+            P += long_to_bytes(int(self._radix), 3)
+            P += b'\x0a'  # always ten rounds
+            P += long_to_bytes(self._split(n) % 256, 1)
             P += long_to_bytes(n, 4)
             P += long_to_bytes(t, 4)
-            self._P[n] = P
+            self._P_cache[n] = P
 
-        if T == 0:
-            Q = ''
+        # Build Q
+        if tweak == 0:
+            Q = b''
         else:
-            Q = str(T)
-            
-        Q += '\x00' * (((-1 * t) - b - 1) % 16)
+            Q = str(tweak).encode('latin-1')
+        
+        Q += b'\x00' * (((-1 * t) - b_bytes - 1) % 16)
         Q += long_to_bytes(i, blocksize=1)
         
-        _B_as_bytes = long_to_bytes(B)
-        Q += '\x00' * (b - len(_B_as_bytes))
-        Q += _B_as_bytes[-b:]
+        b_as_bytes = long_to_bytes(b)
+        Q += b'\x00' * (b_bytes - len(b_as_bytes))
+        Q += b_as_bytes[-b_bytes:] if b_bytes > 0 else b''
 
-        _cbc = AES.new(self._K, AES.MODE_CBC, '\x00' * 16)
+        cbc = AES.new(self._key, AES.MODE_CBC, b'\x00' * 16)
 
-        assert len(self._P[n]) % 16 == 0
+        assert len(self._P_cache[n]) % 16 == 0
         assert len(Q) % 16 == 0
 
-        Y = _cbc.encrypt(self._P[n] + Q)[-16:]
+        Y = cbc.encrypt(self._P_cache[n] + Q)[-16:]
 
-        i = 1
+        # Extend Y if needed
+        j = 1
         TMP = Y
         while len(TMP) < (d + 4):
-            _len = len(Y)
-            X = bytes_to_long(Y) ^ i
-            TMP += self._ecb.encrypt(long_to_bytes(X, blocksize=_len))
-            i += 1
+            Y_len = len(Y)
+            X_val = bytes_to_long(Y) ^ j
+            TMP += self._ecb.encrypt(long_to_bytes(X_val, blocksize=Y_len))
+            j += 1
 
         y = bytes_to_long(TMP[:(d + 4)])
-        z = y % (self._radix ** m)
+        z = y % (int(self._radix) ** m)
 
         return z
 
-    def encrypt(self, T, X):
-        retval = ''
-
-        n = len(X)
-        l = self.split(n)
-        r = 10 #self.rnds(n)
-        A = X[:l]
-        B = X[l:]
-        for i in range(r):
-            C = self.add(A, self.F(n, T, i, B))
+    def encrypt(self, tweak: Union[FFXInteger, int], plaintext: FFXInteger) -> FFXInteger:
+        """Encrypt a plaintext using FFX.
+        
+        Args:
+            tweak: The tweak value (can be FFXInteger or 0 for no tweak)
+            plaintext: The message to encrypt as FFXInteger
+        
+        Returns:
+            Encrypted message as FFXInteger
+        """
+        n = len(plaintext)
+        l = self._split(n)
+        
+        A = plaintext[:l]
+        B = plaintext[l:]
+        
+        for i in range(self.NUM_ROUNDS):
+            C = self._add_mod(A, self._F(n, tweak, i, B))
             A = B
             B = C
 
-        retval = FFXInteger(str(A) + str(B), radix=self._radix)
-        
-        return retval
+        return FFXInteger(str(A) + str(B), radix=int(self._radix))
 
-    def decrypt(self, T, Y):
-        retval = ''
+    def decrypt(self, tweak: Union[FFXInteger, int], ciphertext: FFXInteger) -> FFXInteger:
+        """Decrypt a ciphertext using FFX.
         
-        n = len(Y)
-        l = self.split(n)
-        r = 10 #self.rnds(n)
-
-        A = Y[:l]
-        B = Y[l:]
-        for i in range(r - 1, -1, -1):
+        Args:
+            tweak: The tweak value (must match the one used for encryption)
+            ciphertext: The encrypted message as FFXInteger
+        
+        Returns:
+            Decrypted message as FFXInteger
+        """
+        n = len(ciphertext)
+        l = self._split(n)
+        
+        A = ciphertext[:l]
+        B = ciphertext[l:]
+        
+        for i in range(self.NUM_ROUNDS - 1, -1, -1):
             C = B
             B = A
-            A = self.sub(C, self.F(n, T, i, B))
-            
-        retval = FFXInteger(str(A) + str(B), radix=self._radix)
+            A = self._sub_mod(C, self._F(n, tweak, i, B))
 
-        return retval
+        return FFXInteger(str(A) + str(B), radix=int(self._radix))
